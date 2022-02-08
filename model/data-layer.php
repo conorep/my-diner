4d45f9f4e2f552d57d7e5e576d092e4ed4b346ad
Conor O'Brien <obrien.conor@student.greenriver.edu>
2022-02-08 17:53:12 -0500

    added model directory with a few functions, added order form 3 for drinks

---
+++ model/data-layer.php
@@ -0,0 +1,16 @@
+<?php
+
+function getCondiments()
+{
+ return array('ketchup', 'mustard', 'sriracha', 'mayo', 'salsa');
+}
+
+function getMeal()
+{
+    return array('breakfast','lunch', 'dinner');
+}
+
+function getDrinks()
+{
+    return array('sprite','coke', 'mtn dew');
+}
