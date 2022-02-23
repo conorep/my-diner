e72ad6b554da3474f6062ed4acab85182c5a15a8
Conor O'Brien <obrien.conor@student.greenriver.edu>
2022-02-22 22:17:03 -0500

    made controller, migrated stuff. cool.

--- controller/controller.php
+++ controller/controller.php
@@ -18,4 +18,118 @@
             //like an include
             echo $views->render('views/home.html');
         }
+
+        function order1()
+        {
+            //initialize input variables.
+            $food = "";
+            $meal = '';
+
+
+            $this->_f3->set('meals', DataLayer::getMeal());
+            //if the form has been posted
+            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                $food = $_POST['food'];
+                $meal = $_POST['meal'];
+
+                //instantiate an order object and add to sesh
+                $_SESSION['order'] = new Order();
+
+                //DONE: Validate the data
+                if (Validator::validFood($food)) {
+                    //add data to session variable
+                    $_SESSION['order']->setFood($food);
+
+                    //could do this to not have to session_start
+                    /*        $f3->set('SESSION.food', $f3->get('POST.food'));
+                            $f3->set('SESSION.meal', $f3->get('POST.meal'));*/
+                } else {
+                    $this->_f3->set('errors["food"]', 'Please enter a food!');
+                }
+
+                if (Validator::validMeal($meal)) {
+                    $_SESSION['order']->setMeal($meal);
+                    /*$_SESSION['meal'] = $meal;*/
+                } else {
+                    $this->_f3->set('errors["meal"]', 'Please select a valid meal!');
+                }
+
+                //redirect user to next page if no errors
+                if (empty($this->_f3->get('errors'))) {
+                    $this->_f3->reroute('order2');
+                }
+            }
+
+            $this->_f3->set('food', $food);
+            $this->_f3->set('userMeal', $meal);
+
+            $views = new Template();
+            echo $views->render('views/orderForm1.html');
+        }
+
+        function order2()
+        {
+            //get condiments from model and add to hive
+            $this->_f3->set('conds', DataLayer::getCondiments());
+
+            //if the form has been posted
+            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                //DONE: Validate the data
+                if (Validator::validCondiments($_POST['conds'])) {
+                    //add data to session variable
+                    if (isset($_POST['conds'])) {
+                        $_SESSION['order']->setCondiments(implode(", ", $_POST['conds']));
+                        /*$_SESSION['conds'] = implode(", ", $_POST['conds']);*/
+                    } else {
+                        $_SESSION['order']->setCondiments("None selected.");
+                    }
+
+                    //redirect user to next page
+                    $this->_f3->reroute('order3');
+                } else {
+                    $this->_f3->set('errors["condiments"]', 'Please select valid condiments!');
+                }
+
+            }
+
+            $views = new Template();
+            echo $views->render('views/orderForm2.html');
+        }
+
+        function order3()
+        {
+            $this->_f3->set('drinks', DataLayer::getDrinks());
+            //if the form has been posted
+            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                //add data to session variable
+                $_SESSION['order']->setDrinks($_POST['drink']);
+
+                //redirect user to next page
+                $this->_f3->reroute('summary');
+
+            }
+
+            $views = new Template();
+            echo $views->render('views/orderForm3.html');
+        }
+
+        function summary()
+        {
+            $views = new Template();
+            echo $views->render('views/summary.html');
+            //clear the session data to start again
+            session_destroy();
+        }
+
+        function breakfast()
+        {
+            $views = new Template();
+            echo $views->render('views/breakfast-menu.html');
+        }
+
+        function lunch()
+        {
+            $views = new Template();
+            echo $views->render('views/lunch.html');
+        }
     }
--- index.php
+++ index.php
@@ -8,8 +8,6 @@
     error_reporting(E_ALL);
 
     require_once('vendor/autoload.php');
-    //this is not needed with autoload
-    //require('classes/order.php');
 
     session_start();
     var_dump($_SESSION);
@@ -24,7 +22,6 @@
     {
         //if only using once do this
         $GLOBALS['con']->home();
-
         /*global $con;
         $con->home();*/
     });
@@ -32,126 +29,42 @@
     //define a breakfast route
     $f3->route('GET /breakfast', function ()
     {
-        $views = new Template();
-        echo $views->render('views/breakfast-menu.html');
+        $GLOBALS['con']->breakfast();
     });
 
     //define a lunch route
     $f3->route('GET /lunch', function ()
     {
-        $views = new Template();
-        echo $views->render('views/lunch.html');
+        $GLOBALS['con']->lunch();
     });
 
     //define a route for order 1
-    $f3->route('GET|POST /order1', function ($f3)
+    $f3->route('GET|POST /order1', function ()
     {
-        //initialize input variables.
-        $food = "";
-        $meal = '';
-
-
-        $f3->set('meals', DataLayer::getMeal());
-        //if the form has been posted
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $food = $_POST['food'];
-            $meal = $_POST['meal'];
-
-            //instantiate an order object and add to sesh
-            $_SESSION['order'] = new Order();
-
-            //DONE: Validate the data
-            if (Validator::validFood($food)) {
-                //add data to session variable
-                $_SESSION['order']->setFood($food);
-
-                //could do this to not have to session_start
-                /*        $f3->set('SESSION.food', $f3->get('POST.food'));
-                        $f3->set('SESSION.meal', $f3->get('POST.meal'));*/
-            } else {
-                $f3->set('errors["food"]', 'Please enter a food!');
-            }
-
-            if (Validator::validMeal($meal)) {
-                $_SESSION['order']->setMeal($meal);
-                /*$_SESSION['meal'] = $meal;*/
-            } else {
-                $f3->set('errors["meal"]', 'Please select a valid meal!');
-            }
-
-            //redirect user to next page if no errors
-            if (empty($f3->get('errors'))) {
-                $f3->reroute('order2');
-            }
-        }
-
-        $f3->set('food', $food);
-        $f3->set('userMeal', $meal);
-
-        $views = new Template();
-        echo $views->render('views/orderForm1.html');
+        $GLOBALS['con']->order1();
     });
 
     //define a route for order 2
-    $f3->route('GET|POST /order2', function ($f3)
+    $f3->route('GET|POST /order2', function ()
     {
-        //get condiments from model and add to hive
-        $f3->set('conds', DataLayer::getCondiments());
-
-        //if the form has been posted
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            //DONE: Validate the data
-            if (Validator::validCondiments($_POST['conds'])) {
-                //add data to session variable
-                if (isset($_POST['conds'])) {
-                    $_SESSION['order']->setCondiments(implode(", ", $_POST['conds']));
-                    /*$_SESSION['conds'] = implode(", ", $_POST['conds']);*/
-                } else {
-                    $_SESSION['order']->setCondiments("None selected.");
-                }
-
-                //redirect user to next page
-                $f3->reroute('order3');
-            } else {
-                $f3->set('errors["condiments"]', 'Please select valid condiments!');
-            }
-
-        }
-
-        $views = new Template();
-        echo $views->render('views/orderForm2.html');
+        $GLOBALS['con']->order2();
     });
 
     //define a form3 route
-    $f3->route('GET|POST /order3', function ($f3)
+    $f3->route('GET|POST /order3', function ()
     {
-        $f3->set('drinks', DataLayer::getDrinks());
-        //if the form has been posted
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            //add data to session variable
-            $_SESSION['order']->setDrinks($_POST['drink']);
-
-            //redirect user to next page
-            $f3->reroute('summary');
-
-        }
-
-        $views = new Template();
-        echo $views->render('views/orderForm3.html');
+        $GLOBALS['con']->order3();
     });
 
     //route for summary
     $f3->route('GET /summary', function ()
     {
-        $views = new Template();
-        echo $views->render('views/summary.html');
-        //clear the session data to start again
-        session_destroy();
+        $GLOBALS['con']->summary();
     });
 
     //run fat-free -> invokes
     $f3->run();
-    \
+
 
     /*
      * Conor O'Brien SDEV328
@@ -160,4 +73,4 @@
     */
 
     //send output to the browser
-        ob_flush();
+    ob_flush();
